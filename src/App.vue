d9895b8818048a260e5e90ffa1188f18efee40de
Volodymyr Kladochnyi <kladochnyi.v@gmail.com>
2019-09-30 21:36:44 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PositiveBtn />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PositiveBtn from "./components/PositiveBtn";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    PositiveBtn
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/PositiveBtn.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="positive-btn">Добавити дизайн</button>
+</template>
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.positive-btn {
+  background: #418af2;
+  color: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  box-sizing: border-box;
+  border-radius: 3px;
+  padding: 7px 11px;
+  cursor: pointer;
+  font-size: 14px;
+}
+</style>
