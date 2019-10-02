c32d9adeb230902991fd281ebe51318fc337702e
Volodymyr Kladochnyi <kladochnyi.v@gmail.com>
2019-10-03 02:10:42 +0300

    upd add picture styles

--- src/components/AddPicture.vue
+++ src/components/AddPicture.vue
@@ -7,7 +7,12 @@
       <div class="picture-item" v-for="img in images" :key="img.id">
         <img class="picture-item__img" :src="img.src" />
       </div>
-      <input type="file" @change="addNewImg" />
+      <div class="add-picture__new">
+        <input type="file" @change="addNewImg" id="file" />
+        <label for="file">
+          <img src="@/assets/svg/add.svg" />
+        </label>
+      </div>
     </div>
   </div>
 </template>
@@ -45,6 +50,39 @@ export default {
     justify-content: center;
     align-items: center;
     cursor: pointer;
+  }
+  &__images {
+    display: flex;
+  }
+  &__new {
+    input {
+      width: 0px;
+      height: 0px;
+      position: absolute;
+    }
+    label {
+      width: 120px;
+      height: 120px;
+      border: 2px solid rgba(0, 0, 0, 0.1);
+      border-style: dotted;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+}
+.picture-item {
+  margin-right: 8px;
+  &__img {
+    max-width: 120px;
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    box-sizing: border-box;
+    border-radius: 3px;
+    &:hover {
+      box-shadow: 0px 8px 20px rgba(0, 0, 0, 0.15);
+      cursor: pointer;
+    }
   }
 }
 </style>
